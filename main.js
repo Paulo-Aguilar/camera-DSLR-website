9fb93d81ceb55fc399f8215d737bd1d0d3de25b3
Paulo Aguilar <paulo.aguilar92@gmail.com>
2021-12-15 11:47:41 +0800

    added hamburger menu

---
+++ main.js
@@ -0,0 +1,11 @@
+const menu_btn = document.querySelector('.menuBtn');
+let menu_open = false;
+menu_btn.addEventListener('click', ()=>{
+    if(!menu_open){
+        menu_btn.classList.add('open');
+        menu_open=true;
+    }else{
+        menu_btn.classList.add('open');
+        menu_open=false;
+    }
+});
